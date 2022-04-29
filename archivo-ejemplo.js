91981a3587313adefc20d9c3cbcc1a4c7fc27b71
Your Name <ypaolaortiz395@gmail.com>
2022-04-28 23:38:00 -0500

    esta es mi primera vez

--- archivo-ejemplo.js
+++ archivo-ejemplo.js
@@ -1,1 +1,11 @@
-hola amor
+1 paso 
+modificar el archivo o hacer los cambios 
+2 paso
+realizar it add 
+3 paso 
+realizar git commit 
+4 paso 
+se abre para pone run comentario d el commit 
+5 paso 
+revisar si dice aborting o success en el commit 
+6
